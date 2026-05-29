7e8ec0a8caf94a0f8564863c12c251998f05490c
Irmuun-bcc <30067717@cc.peralta.edu>
2026-05-29 07:49:39 +0000

    o

---
+++ Player.h
@@ -0,0 +1,30 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    string name;
+    int health;
+    int maxHealth;
+    int attackPower;
+
+public:
+    Player();
+    Player(string name, int health, int attackPower);
+    ~Player();
+
+    void takeDamage(int damage);
+
+    string getName() const;
+    int getHealth() const;
+    int getMaxHealth() const;
+    int getAttackPower() const;
+
+    void displayStatus() const;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Player hero;
+    Player wizard("Gandalf", 120, 15);
+
+    cout << endl;
+    cout << "--- Initial Status ---" << endl;
+    hero.displayStatus();
+    wizard.displayStatus();
+
+    cout << endl;
+    cout << "--- Hero takes damage! ---" << endl;
+    hero.takeDamage(25);
+    hero.displayStatus();
+
+    cout << endl;
+    cout << "--- Wizard takes damage! ---" << endl;
+    wizard.takeDamage(150);
+    wizard.displayStatus();
+
+    cout << endl;
+
+    return 0;
+}
